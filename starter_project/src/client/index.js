64fc78fa2fbe6fad9006dced8737206d5cc2a2f9
Ali Helmi <ahelmi365@gmail.com>
2024-11-19 19:54:59 +0200

    import scss

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -6,5 +6,10 @@ alert("I EXIST");
 // console.log("CHANGE!!");
 
 // sass files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 
 export { checkForName, handleSubmit };
