44983ca2517310ea58b77619f7c34a996077c06d
Ali Helmi <ahelmi365@gmail.com>
2024-11-20 17:23:07 +0200

    test run dev

--- starter_project/src/client/js/formHandler.js
+++ starter_project/src/client/js/formHandler.js
@@ -1,30 +1,52 @@
 // Replace checkForName with a function that checks the URL
-import { checkForName } from './nameChecker'
+import { checkForName } from "./nameChecker";
 
 // If working on Udacity workspace, update this with the Server API URL e.g. `https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api`
 // const serverURL = 'https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api'
-const serverURL = 'https://localhost:8000/api'
+const serverURL = "https://localhost:8000/api";
 
-const form = document.getElementById('urlForm');
-form.addEventListener('submit', handleSubmit);
+const form = document.getElementById("urlForm");
+form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
+  makeRequest();
+  // Get the URL from the input field
+  const formText = document.getElementById("name").value;
 
-    // Get the URL from the input field
-    const formText = document.getElementById('name').value;
+  // This is an example code that checks the submitted name. You may remove it from your code
+  //   checkForName(formText);
 
-    // This is an example code that checks the submitted name. You may remove it from your code
-    checkForName(formText);
-    
-    // Check if the URL is valid
- 
-        // If the URL is valid, send it to the server using the serverURL constant above
-      
+  // Check if the URL is valid
+
+  // If the URL is valid, send it to the server using the serverURL constant above
 }
+
+const makeRequest = () => {
+   const formdata = new FormData();
+  formdata.append("key", "baff2a8e891118765dce51191d26e648");
+  formdata.append("txt", "I love vodfone company, they have amazing services");
+  formdata.append("lang", "es"); // 2-letter code, like en es fr ...
+
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+
+  const response = fetch(
+    "https://api.meaningcloud.com/sentiment-2.1",
+    requestOptions
+  )
+    .then((response) => ({
+      status: response.status,
+      body: response.json(),
+    }))
+    .then(({ status, body }) => console.log(status, body))
+    .catch((error) => console.log("error", error));
+};
 
 // Function to send data to the server
 
 // Export the handleSubmit function
 export { handleSubmit };
-
--- starter_project/src/server/index.js
+++ starter_project/src/server/index.js
@@ -1,13 +1,13 @@
-var path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
-const cors = require('cors');
-app.use(express.static('dist'))
+const cors = require("cors");
+app.use(express.static("dist"));
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -15,22 +15,17 @@ console.log(__dirname);
 
 // Variables for url and api key
 // You could call it aylienapi, or anything else
-var textapi = new aylien({
-    application_key: process.env.API_KEY
-  });
+const textapi = {
+  application_key: process.env.API_KEY,
+};
 
-app.get('/', function (req, res) {
-    res.send("./dist/index.html")
+app.get("/", function (req, res) {
+  res.send("./dist/index.html");
 });
-
 
 // POST Route
 
-
-
 // Designates what port the app will listen to for incoming requests
 app.listen(8000, function () {
-    console.log('Example app listening on port 8000!');
+  console.log("Example app listening on port 8000!");
 });
-
-
